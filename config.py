5b0fc551c7bbf9acc8be2f8314c3fa213ff3abd5
anuraghithub-rgb <anuraghithub@gmail.com>
2026-05-19 00:03:42 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+{
+    "bot_token": "YOUR_BOT_TOKEN_FROM_BOTFATHER",
+    "github_token": "github_pat_YOUR_GITHUB_TOKEN_HERE",
+    "github_repo": "YOUR_USERNAME/YOUR_REPO_NAME",
+    "owner_id": 123456789,
+    "admins": [],
+    "user_balances": {},
+    "redeem_keys": {}
+}
